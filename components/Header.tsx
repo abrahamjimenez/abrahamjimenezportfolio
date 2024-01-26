0e5ec5a16f2351d8ceefc79f14fc160d94018667
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-01-26 14:20:12 -0700

    Animation on links & icons in Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,10 +8,14 @@ export default function Header() {
       <h2 className="text-2xl font-sans">Full-stack developer</h2>
       <div className="flex gap-4 pb-4">
         <a href="https://github.com/abrahamjimenez" target="_blank">
-          <FontAwesomeIcon icon={faGithub} className="w-5 h-5 " />
+          <FontAwesomeIcon
+            icon={faGithub}
+            className="w-5 h-5 hover:cursor-pointer hover:scale-150 hover:text-blue-500"
+          />
         </a>
         <a
           href="https://www.linkedin.com/in/abrahamjimenezromo/"
+          className="w-5 h-5 hover:cursor-pointer hover:scale-150 hover:text-blue-500"
           target="_blank"
         >
           <FontAwesomeIcon icon={faLinkedin} className="w-5 h-5" />
