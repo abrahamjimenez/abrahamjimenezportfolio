ec757202b79d8531164d5c11713808fad81fc4ab
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-01-27 00:08:47 -0700

    Gave max content to all a tags

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
